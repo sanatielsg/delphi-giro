aa06c151da7194b4f60fd4bb46d5ff86fa7aa479
Sanatiel <sanatielsg@gmail.com>
2025-01-04 00:04:37 -0300

    modeling Local

--- Model/Entity/ULocal.pas
+++ Model/Entity/ULocal.pas
@@ -2,6 +2,18 @@ unit ULocal;
 
 interface
 
+type
+TLocal = class
+  private
+    FCodigo : Integer;
+    FDescricao : String;
+    FStatus: Integer;
+  public
+    property Codigo : Integer   read FCodigo write FCodigo ;
+    property Descricao : String read FDescricao write FDescricao ;
+    property Status: Integer    read FStatus write FStatus ;
+end;
+
 implementation
 
 end.
